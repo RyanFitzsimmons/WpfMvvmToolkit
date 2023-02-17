b2276bca26242cbd3a6b8c73f9150a1f10c272f4
Ryan Fitzsimmons <ryan.fitzsimmons@outlook.com>
2023-02-17 09:40:31 +0000

    Added loaded and not loaded exceptions

---
+++ WpfMvvmToolkit/Exceptions/AlreadyLoadedException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace WpfMvvmToolkit.Exceptions
+{
+    public class AlreadyLoadedException : Exception
+    {
+        public AlreadyLoadedException(string className)
+            : base($"The {className} is already loaded.")
+        {
+
+        }
+
+        public static void ThrowIf(bool isLoaded, [System.Runtime.CompilerServices.CallerFilePath] string className = "")
+        {
+            if (isLoaded)
+            {
+                Throw(Path.GetFileNameWithoutExtension(className));
+            }
+        }
+
+        private static void Throw(string className)
+        {
+            throw new AlreadyLoadedException(className);
+        }
+    }
+}
---
+++ WpfMvvmToolkit/Exceptions/NotLoadedException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace WpfMvvmToolkit.Exceptions
+{
+    public class NotLoadedException : Exception
+    {
+        public NotLoadedException(string className)
+            : base($"The {className} is not loaded.")
+        {
+
+        }
+
+        public static void ThrowIfNot(bool isLoaded, [System.Runtime.CompilerServices.CallerFilePath] string className = "")
+        {
+            if (!isLoaded)
+            {
+                Throw(Path.GetFileNameWithoutExtension(className));
+            }
+        }
+
+        private static void Throw(string className)
+        {
+            throw new NotLoadedException(className);
+        }
+    }
+}
