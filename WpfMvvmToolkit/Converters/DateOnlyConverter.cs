f69f9e0daabb7f4511f6951e9e3380f0e5cd7541
Fitzsimmons, Ryan (CIC) <P10177829@capita.co.uk>
2023-06-29 11:42:00 +0100

    made changed to date and time converters

---
+++ WpfMvvmToolkit/Converters/DateOnlyConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows;
+
+namespace WpfMvvmToolkit.Converters;
+public class DateOnlyConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        var dateTime = (DateOnly?)value;
+        if (!dateTime.HasValue)
+        {
+            return string.Empty;
+        }
+        var format = (string)parameter;
+        return dateTime.Value.ToString(format, culture);
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        var format = (string)parameter;
+        DateOnly date;
+        if (DateOnly.TryParseExact((string)value, format, culture, DateTimeStyles.None, out date))
+        {
+            return date;
+        }
+        return DependencyProperty.UnsetValue;
+    }
+}
